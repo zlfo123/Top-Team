ed040b85e3519f47d768cb3c134bcc21fb6889f6
zlfo123 <136383321+zlfo123@users.noreply.github.com>
2023-08-08 15:11:44 +0300

    Update and rename zaid shbeeb .css to java.js

---
+++ java.js
@@ -0,0 +1,47 @@
+let head = document.getElementById('head');
+let head2 = document.getElementById('head2');
+let btn = document.getElementById('btn');
+let btn2 = document.getElementById('btn2');
+
+
+
+let i = 5;
+
+btn.onclick = function () {
+    setInterval(
+        function(){
+            head2.innerHTML = 'please wait'
+            head.innerHTML = i--;
+            while(i < -1){
+                clearInterval(this);
+                setTimeout(
+                    location.assign('https://www.youtube.com/watch?v=I0j9-5IVSC4'),500
+                )
+            }
+        },1000
+       );
+    
+} 
+
+ 
+btn2.onclick = function () {
+    setInterval(
+        function(){
+            head2.innerHTML = 'please wait'
+            head.innerHTML = i--;
+            while(i < -1){
+                clearInterval(this);
+                let img = document.createElement('img')
+                setTimeout(
+                    location.assign(''),500
+                )
+            }
+        },1000
+       );
+    
+} 
+
+
+   
+
+   
